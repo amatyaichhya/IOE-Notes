b80d6df280bf8a56f539b3b2e082ba84326cadfd
amatyaichhya <amatyaichhya@gmail.com>
2019-01-13 10:33:05 +0545

    ioe mainpage layout

---
+++ app/src/main/java/com/nayaproject/ioenotes/MainActivity.java
@@ -0,0 +1,67 @@
+package com.nayaproject.ioenotes;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialSearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.closeSearch();
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        searchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
+        searchView.setCursorDrawable(R.drawable.custom_cursor);
+
+
+        return true;
+    }
+
+}
